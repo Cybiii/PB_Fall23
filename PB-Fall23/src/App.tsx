e353d465749f82133c475a0c0a0bcc5b369654bd
Sebastian <110711768+Cybiii@users.noreply.github.com>
2023-10-26 17:14:19 -0700

    update

--- PB-Fall23/src/App.tsx
+++ PB-Fall23/src/App.tsx
@@ -2,13 +2,36 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Tabs from './components/Tabs'
+import { Fragment } from 'react'
+import { Tab } from '@headlessui/react'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1>Poop</h1>
+    <Tab.Group>
+      <Tab.List>
+        <Tab as={Fragment}>
+          {({ selected }) => (
+            /* Use the `selected` state to conditionally style the selected tab. */
+            <button
+              className={
+                selected ? 'bg-blue-500 text-white' : 'bg-white text-black'
+              }
+            >
+              Tab 1
+            </button>
+          )}
+        </Tab>
+        {/* ...  */}
+      </Tab.List>
+      <Tab.Panels>
+        <Tab.Panel>Content 1</Tab.Panel>
+        {/* ... */}
+      </Tab.Panels>
+    </Tab.Group>
     </>
   )
 }
