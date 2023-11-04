2baa5f8d3622379d3fc714c2de8bb0cf77d1396d
Sebastian <110711768+Cybiii@users.noreply.github.com>
2023-11-03 17:26:11 -0700

    Modified Navbar

--- PB-Fall23/src/components/Navbar.tsx
+++ PB-Fall23/src/components/Navbar.tsx
@@ -2,30 +2,25 @@ import React from 'react';
 
 const Navbar = () => {
   return (
-    <nav className="bg-gray-900 text-white p-4 shadow-md">
-      <div className="container mx-auto flex justify-between items-center">
+    <nav className="bg-gray-900 text-white 4xl p-4 shadow-md">
+      <div className="container mx-auto flex py-3 pl-10 justify-between items-center">
         <h1 className="text-3xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-blue-400 to-teal-300">
           SEXO
         </h1>
-        <ul className="flex space-x-4 md:space-x-6">
-          <li>
-            <a href="#hero" className="text-sm md:text-base hover:text-blue-400 transition-colors duration-300">
-              Home
-            </a>
-          </li>
+        <ul className="flex space-x-20 px-10 md:space-x-9">
           <li>
             <a href="#projects" className="text-sm md:text-base hover:text-blue-400 transition-colors duration-300">
-              Projects
+              About Us
             </a>
           </li>
           <li>
             <a href="#about" className="text-sm md:text-base hover:text-blue-400 transition-colors duration-300">
-              About
+              Contact
             </a>
           </li>
           <li>
             <a href="#contact" className="text-sm md:text-base hover:text-blue-400 transition-colors duration-300">
-              Contact
+              Resources
             </a>
           </li>
         </ul>
